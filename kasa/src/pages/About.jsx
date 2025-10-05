fcea2df88e76916bbc1e01376cf6d7324dcd9c3c
Onnichad <treil.christopher@outlook.com>
2025-10-05 16:30:47 -0400

    pages set up

--- kasa/src/pages/About.jsx
+++ kasa/src/pages/About.jsx
@@ -1,7 +1,7 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Accordion from "../components/Accordion";
-import aboutBanner from "../assets/backgroundpropos.png";
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Accordion from '../components/Accordion';
+import aboutBanner from '../assets/backgroundpropos.png';
 
 export default function About() {
   return (
